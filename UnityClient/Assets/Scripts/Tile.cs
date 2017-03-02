90addd919f69ff94d87a840e2a633412764b2280
lee <lee.ringham@gmail.com>
2017-03-01 20:34:24 -0700

    Added world and tile classes Commit to sync with other computer

---
+++ UnityClient/Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    enum TileType
+    {
+        Dirt,
+        Empty
+    };
+
+    TileType type = TileType.Empty;
+    World world;
+    int x, y;
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }   
+}
---
+++ UnityClient/Assets/Scripts/World.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+    Tile[,] tiles;
+    int width, height;
+
+    public World(int width = 100, int height = 100)
+    {
+        this.width = width;
+        this.height = height;
+        tiles = new Tile[width, height];
+
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < height; ++y)
+                tiles[x, y] = new Tile(this, x, y);
+    }
+
+    public Tile getTileAt(int x, int y)
+    {
+
+    }
+}
