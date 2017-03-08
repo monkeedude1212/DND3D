ffbbc26772a122e841f338b86540f40c15a738ed
MSI\Adam <adamjahraus@gmail.com>
2017-03-07 17:09:05 -0700

    Defined Human race

---
+++ UnityClient/Assets/Scripts/Human.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Human : Race {
+
+    public override string raceName()
+    {
+        return "Human";
+    }
+
+    Human()
+    {
+        base.bonusAttributes = new int[6];
+
+        for (int i = 0; i < 6; i++)
+        {
+            base.bonusAttributes[i] = 1;
+        }
+    }
+}
