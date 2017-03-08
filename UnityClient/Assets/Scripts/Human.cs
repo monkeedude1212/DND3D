a086ff1c792f5eb578c4d4624d5cfce00e1c0a40
MSI\Adam <adamjahraus@gmail.com>
2017-03-07 17:37:30 -0700

    Added comments to Human

--- UnityClient/Assets/Scripts/Human.cs
+++ UnityClient/Assets/Scripts/Human.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 
 public class Human : Race {
 
+    // Abstract class? Not certain this is the best way of doing this
     public override string raceName()
     {
         return "Human";
     }
 
+    // Constructor for race Human
     Human()
     {
+        // Humans get +1 to all attributes
         base.bonusAttributes = new int[6];
 
         for (int i = 0; i < 6; i++)
@@ -17,8 +20,10 @@ public class Human : Race {
             base.bonusAttributes[i] = 1;
         }
 
+        // Humans are medium sized
         base.raceSize = Size.Medium;
 
+        // Their base movement rate is 30ft / round
         base.raceSpeed = 30;
     }
 }
