1261307baf8d4c85b9a3d6adac16a57600610dd5
MSI\Adam <adamjahraus@gmail.com>
2017-03-07 17:08:14 -0700

    Converted raceName to an abstract function

--- UnityClient/Assets/Scripts/Race.cs
+++ UnityClient/Assets/Scripts/Race.cs
@@ -7,12 +7,14 @@ public abstract class Race {
     // There can be many different races in D&D, including sub-races
     // This list should be expandable
     // Maybe Race should be an abstract class? Probably a reasonable approach
-    
-    private string raceName;
+
+    public abstract string raceName();
 
     // This probably shouldn't be defined as an array, but it's 
     // probably okay for now. 
-    private int[] bonusAttributes;
+    protected int[] bonusAttributes;
+
+    enum Size { Fine, Diminutive, Tiny, Small, Medium, Large, Huge, Gargantuan, Colossal};
 
     // We don't have these things defined yet, but they will become necessary 
     // private Ability[] raceAbilities;
