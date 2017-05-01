c78595fe247813e05998f8e285a18341b734fbb5
ajahraus <adamjahraus@gmail.com>
2017-04-30 21:41:49 -0600

    Formatting changes to character class

--- UnityClient/Assets/Scripts/Character.cs
+++ UnityClient/Assets/Scripts/Character.cs
@@ -1,55 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Character{
+namespace models
+{
 
-    // These are the basic attributes
-    public int strength { get; protected set; }
-    public int dexterity { get; protected set; }
-    public int constitution { get; protected set; }
-    public int intelligence { get; protected set; }
-    public int wisdom { get; protected set; }
-    public int charisma { get; protected set; }
-    
-    // These are the numbers that actually matter, derived from
-    // the attributes
-    public int strMod { get; private set; }
-    public int dexMod { get; private set; }
-    public int conMod { get; private set; }
-    public int intMod { get; private set; }
-    public int wisMod { get; private set; }
-    public int chaMod { get; private set; }
+	public class Character : MonoBehaviour
+	{
+		// These are the basic attributes
+		public int strength { get; protected set; }
 
-    // Character has a race
-    private Race race;
+		public int dexterity { get; protected set; }
 
-    //private Alignment alignment;
+		public int constitution { get; protected set; }
 
-    //private CharacterClass cc;
+		public int intelligence { get; protected set; }
 
-    public uint age { get; private set; }
+		public int wisdom { get; protected set; }
 
-    // This is the value of maximum possible hit points. This should be
-    // replaced by a dedicated hit point interface later on
-    public uint maxHitPoints { get; private set; }
+		public int charisma { get; protected set; }
 
-    public uint playerLevel { get; private set; }
+		// These are the numbers that actually matter, derived from
+		// the attributes
+		public int strMod { get; private set; }
 
-    public uint currentXP { get; private set; }
-    
-    public bool hasInspiration { get; private set; }
+		public int dexMod { get; private set; }
 
-    public uint speed { get; private set; }
+		public int conMod { get; private set; }
 
-    Character () {
-        // This is how modifiers are calculated from attruibutes
-        // They should probably have an "update" function
-		strMod = (int) (((float)strength / 2.0) - 5.0);
-        dexMod = (int) (((float)dexterity / 2.0) - 5.0);
-        conMod = (int) (((float)constitution / 2.0) - 5.0);
-        intMod = (int) (((float)intelligence / 2.0) - 5.0);
-        wisMod = (int) (((float)wisdom / 2.0) - 5.0);
-        chaMod = (int) (((float)charisma/ 2.0) - 5.0);
-    }
-	
+		public int intMod { get; private set; }
+
+		public int wisMod { get; private set; }
+
+		public int chaMod { get; private set; }
+
+		// Character has a race
+		private Race race;
+
+		//private Alignment alignment;
+
+		//private CharacterClass cc;
+
+		public uint age { get; private set; }
+
+		// This is the value of maximum possible hit points. This should be
+		// replaced by a dedicated hit point interface later on
+		public uint maxHitPoints { get; private set; }
+
+		public uint playerLevel { get; private set; }
+
+		public uint currentXP { get; private set; }
+
+		public bool hasInspiration { get; private set; }
+
+		public uint speed { get; private set; }
+
+		public string name;
+
+		Character ()
+		{
+			// This is how modifiers are calculated from attruibutes
+			// They should probably have an "update" function
+			strMod = (int)(((float)strength / 2.0) - 5.0);
+			dexMod = (int)(((float)dexterity / 2.0) - 5.0);
+			conMod = (int)(((float)constitution / 2.0) - 5.0);
+			intMod = (int)(((float)intelligence / 2.0) - 5.0);
+			wisMod = (int)(((float)wisdom / 2.0) - 5.0);
+			chaMod = (int)(((float)charisma / 2.0) - 5.0);
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+			DontDestroyOnLoad (this);
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+
+		}
+
+		public void setName (string newName)
+		{
+			name = newName;
+		}
+	}
 }
